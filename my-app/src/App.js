f903643a09c0f11da6432767aee5c68a1fe1a9ea
Nicolás Mannarino <nicolas.mannarino@hotmail.com>
2023-01-21 01:25:55 -0300

    Add button "Pokemoncito" and image of the pokemon

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  
+  //var cont = 0;
+  /*const [cont, setCont] = useState(0);
+  function devolverCont()
+  {
+    setCont(cont+1);
+    console.log("Funcion Contador: " + cont);
+  }
+  console.log("Contador Afu: " + cont);
+*/
+const [pokemon, setPokemon] = useState();
+
+useEffect(() => {
+  getPokemon(151);
+}, []);
+
+async function getPokemon(idPokemon)
+{
+    const urlPokemon = `https://pokeapi.co/api/v2/pokemon/${idPokemon}/`;
+    const response = await fetch(urlPokemon);
+    const datosPokemon = await response.json();
+    setPokemon(datosPokemon);
+    console.log(datosPokemon);
+
+    //return datosPokemon;  
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>contador: {cont}</h1>
+      <button onClick = {devolverCont}>Aumentar Contador</button> */}
+      
+      {pokemon
+        ?<div><img src={pokemon.sprites.front_default}></img> 
+        <h1 className='nombrePokemon'>{pokemon.name}</h1></div>
+        :null}
+
+      <button onClick={() => getPokemon(Math.floor(Math.random() * (700-1 + 1)) + 1)}>Pokemoncito</button>
+
     </div>
   );
 }
